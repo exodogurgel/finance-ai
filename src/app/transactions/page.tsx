81c9454fbebc5a21bdb816a6a46be6e8920dbe5e
Êxodo Gurgel <exodowellis@gmail.com>
2024-12-09 10:32:57 -0300

    feat: create add transaction dialog

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { db } from "@/lib/prisma";
-import { ArrowDownUpIcon } from "lucide-react";
 import { TransactionsColumns } from "./_columns";
+import { AddTransactionButton } from "@/components/add-transaction-button";
 
 export default async function TransactionsPage() {
   const transactions = await db.transaction.findMany({});
@@ -10,9 +9,7 @@ export default async function TransactionsPage() {
     <div className="space-y-6 p-6">
       <div className="flex w-full items-center justify-between">
         <h1 className="text-2xl font-bold">Transações</h1>
-        <Button className="rounded-full font-bold text-white">
-          Adicionar transação <ArrowDownUpIcon />
-        </Button>
+        <AddTransactionButton />
       </div>
 
       <DataTable columns={TransactionsColumns} data={transactions} />
---
+++ src/constants/transactions.ts
@@ -0,0 +1,116 @@
+import {
+  transactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+
+export const TRANSACTION_CATEGORY_LABELS = {
+  EDUCATION: "Educação",
+  ENTERTAINMENT: "Entretenimento",
+  FOOD: "Alimentação",
+  HEALTH: "Saúde",
+  HOUSING: "Moradia",
+  OTHER: "Outros",
+  SALARY: "Salário",
+  TRANSPORTATION: "Transporte",
+  UTILITY: "Utilidades",
+};
+
+export const TRANSACTION_PAYMENT_METHOD_LABELS = {
+  BANK_TRANSFER: "Transferência Bancária",
+  BANK_SLIP: "Boleto Bancário",
+  CASH: "Dinheiro",
+  CREDIT_CARD: "Cartão de Credito",
+  DEBIT_CARD: "Cartão de Débito",
+  OTHER: "Outros",
+  PIX: "Pix",
+};
+
+export const TRANSACTION_TYPE_OPTIONS = [
+  {
+    value: TransactionType.EXPENSE,
+    label: "Despesa",
+  },
+  {
+    value: TransactionType.DEPOSIT,
+    label: "Depósito",
+  },
+  {
+    value: TransactionType.INVESTMENT,
+    label: "Investimento",
+  },
+];
+
+export const TRANSACTION_PAYMENT_METHOD_OPTIONS = [
+  {
+    value: TransactionPaymentMethod.BANK_TRANSFER,
+    label:
+      TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.BANK_TRANSFER],
+  },
+  {
+    value: TransactionPaymentMethod.BANK_SLIP,
+    label:
+      TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.BANK_SLIP],
+  },
+  {
+    value: TransactionPaymentMethod.CASH,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.CASH],
+  },
+  {
+    value: TransactionPaymentMethod.CREDIT_CARD,
+    label:
+      TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.CREDIT_CARD],
+  },
+  {
+    value: TransactionPaymentMethod.DEBIT_CARD,
+    label:
+      TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.DEBIT_CARD],
+  },
+  {
+    value: TransactionPaymentMethod.OTHER,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.OTHER],
+  },
+  {
+    value: TransactionPaymentMethod.PIX,
+    label: TRANSACTION_PAYMENT_METHOD_LABELS[TransactionPaymentMethod.PIX],
+  },
+];
+
+export const TRANSACTION_CATEGORY_OPTIONS = [
+  {
+    value: transactionCategory.EDUCATION,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.EDUCATION],
+  },
+  {
+    value: transactionCategory.ENTERTAINMENT,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.ENTERTAINMENT],
+  },
+  {
+    value: transactionCategory.FOOD,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.FOOD],
+  },
+  {
+    value: transactionCategory.HEALTH,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.HEALTH],
+  },
+  {
+    value: transactionCategory.HOUSING,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.HOUSING],
+  },
+  {
+    value: transactionCategory.OTHER,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.OTHER],
+  },
+  {
+    value: transactionCategory.SALARY,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.SALARY],
+  },
+  {
+    value: transactionCategory.TRANSPORTATION,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.TRANSPORTATION],
+  },
+  {
+    value: transactionCategory.UTILITY,
+    label: TRANSACTION_CATEGORY_LABELS[transactionCategory.UTILITY],
+  },
+];
